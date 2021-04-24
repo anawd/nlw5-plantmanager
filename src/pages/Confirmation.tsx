331e626b6ee0dc1ce58e2c2b84867a6480080a5a
Ana Paula Moraes <ana@anawd.com>
2021-04-24 10:43:46 -0300

    Aula 3 Finalizada

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/core';
 import React from 'react';
 import {
   SafeAreaView,
@@ -13,6 +14,12 @@ import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 export function Confirmation() {
+  const navigation = useNavigation();
+
+  function handleMoveOn() {
+    navigation.navigate('PlantSelect');
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
@@ -28,6 +35,7 @@ export function Confirmation() {
         <View style={styles.footer}>
           <Button
             title="Começar"
+            onPress={handleMoveOn}
           />
         </View>
       </View>
--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -5,6 +5,7 @@ import colors from '../styles/colors';
 import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
+import { PlantSelect } from '../pages/PlantSelect';
 
 const stackRoutes = createStackNavigator();
 
@@ -31,6 +32,11 @@ const AppRoutes: React.FC = () => (
     <stackRoutes.Screen
       name="Confirmation"
       component={Confirmation}
+    />
+
+    <stackRoutes.Screen
+      name="PlantSelect"
+      component={PlantSelect}
     />
 
   </stackRoutes.Navigator>
